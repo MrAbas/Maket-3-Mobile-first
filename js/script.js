d14b9dbdc38ffc3e950216d0dbbff241a89825e5
MrAbas <abasov2016@list.ru>
2023-12-22 10:09:26 +0300

    last change

--- js/script.js
+++ js/script.js
@@ -1,7 +1,21 @@
 const burgerMenu = document.getElementById("burger__menu");
 const burgerContent = document.getElementById("burger__content");
 
+const buttonSearch = document.getElementById("button__search");
+const containerSearch = document.getElementById("containerSearch");
+const closedSearch = document.getElementById("closedSearch");
+
 burgerMenu.onclick = function () {
   burgerContent.classList.toggle("burger__content");
   burgerMenu.classList.toggle("burger__menu__cross");
+};
+
+buttonSearch.onclick = function () {
+  buttonSearch.style.display = "none";
+  containerSearch.classList.add("container__search__show");
+};
+
+closedSearch.onclick = function () {
+  buttonSearch.style.display = "block";
+  containerSearch.classList.remove("container__search__show");
 };
